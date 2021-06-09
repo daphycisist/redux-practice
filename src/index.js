d8119a13906ed1b5250f54b22bf07f504f0b595a
daphycisist <daphycisist@gmail.com>
2021-06-09 22:28:42 +0100

    implement Ducks pattern folder structure for redux

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
-// import { bugAdded, bugResolved } from "./actions";
-// import store from "./store";
+import configureStore from "./store/configureStore";
+import * as actions from "./store/bugs";
 
-// store.dispatch(bugAdded("Bug1"));
-// store.dispatch(bugResolved(1));
-
-// console.log(store.getState());
-
-import store from "./customStore.js";
-import * as actions from "./actions.js";
+const store = configureStore();
 
 store.subscribe(() => console.log("Store Updated"));
 
 store.dispatch(actions.bugAdded("Bug 1"));
+console.log(store.getState());
+
+store.dispatch(actions.bugAdded("Bug 2"));
 console.log(store.getState());
 
 store.dispatch(actions.resolvedBug(1));
